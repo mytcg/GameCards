1ca057695c2b8e175c022de2f7fb63051e59b4b6
heinzs <heinz@mytcg.net>
2012-08-08 16:04:16 +0200

    cole's fault

--- SARugbyCards/index/index.php
+++ SARugbyCards/index/index.php
@@ -2456,14 +2456,14 @@ if ($_GET['getdecks']){
 
 /** give all the user decks */
 if ($_GET['getalldecks']){
-	$aCompDeckDetails=myqu('SELECT competitiondeck_id, description 
+	$aCompDeckDetails=myqu('SELECT competitiondeck_id, description, imageserver_id, image 
 	FROM mytcg_competitiondeck 
 	WHERE active = "1" 
 	AND competitiondeck_id NOT IN (SELECT competitiondeck_id FROM mytcg_deck WHERE user_id='.$iUserID.' AND type = 2)');
 	$iCount=0;
 	while ($aCompDeckDetail=$aCompDeckDetails[$iCount]){
-		myqui('INSERT INTO mytcg_deck (user_id, category_id, description, type, competitiondeck_id) 
-		VALUES('.$iUserID.',1,"'.trim($aCompDeckDetail['description']).'",2,'.trim($aCompDeckDetail['competitiondeck_id']).')');
+		myqui('INSERT INTO mytcg_deck (user_id, category_id, imageserver_id, description, image, type, competitiondeck_id) 
+		VALUES('.$iUserID.',1,'.(trim($aCompDeckDetail['imageserver_id'])==''?'NULL':trim($aCompDeckDetail['imageserver_id'])).',"'.trim($aCompDeckDetail['description']).'",'.(trim($aCompDeckDetail['image'])==''?'NULL':trim($aCompDeckDetail['image'])).',2,'.trim($aCompDeckDetail['competitiondeck_id']).')');
 		$iCount++;
 	}
 
