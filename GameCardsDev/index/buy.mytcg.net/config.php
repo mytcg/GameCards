64beedfb6fb6f85119460005d442dfad268dc262
jsinclair <james@mytcg.net>
2012-11-15 12:01:18 +0200

    the php site for buy.mytcg.net, and the php for _sms.
    Signed-off-by: jsinclair <james@mytcg.net>

---
+++ GameCardsDev/index/buy.mytcg.net/config.php
@@ -0,0 +1,35 @@
+<?php
+$Conf=array();
+/** GLOBAL SETTINGS HERE, for administrator to modify */
+//Point to dev
+
+// $Conf["database"]["host"]="localhost";
+// $Conf["database"]["databasename"]="gamecard";
+// $Conf["database"]["username"]="root";
+// $Conf["database"]["password"]="";
+// $Conf["database"]["table_prefix"]="mytcg";
+
+$Conf["database"]["host"]="dedi94.flk1.host-h.net";
+$Conf["database"]["databasename"]="gamecard";
+$Conf["database"]["username"]="mytcg_root";
+$Conf["database"]["password"]="g4m3c4rd98";
+$Conf["database"]["table_prefix"]="mytcg";
+
+$Conf["files"]["directory_root"]="";
+$Conf["files"]["directory_var"]="var/";
+$Conf["files"]["file_sql_log"]="sqlq.log";
+
+$Conf["system"]["session_expire_in_minutes"]="20";
+$Conf["system"]["ssl_is_on"]="0";
+$Conf["system"]["rootdir"]="";
+
+/* NO MORE CONFIGURABLE SETTINGS BELOW THIS LINE */		
+$sHTTP = ($Conf["system"]["ssl_is_on"]=="1") ? "https://" : "http://"; 
+$Conf["system"]["web_url"]=$sHTTP.$_SERVER["HTTP_HOST"];
+$Conf["user"]["logged_in"]="0";
+$Conf["user"]["ip"]=$_SERVER["REMOTE_ADDR"];
+$Conf["user"]["user_agent"]=$_SERVER["HTTP_USER_AGENT"];
+
+ini_set("session.gc_maxlifetime",intval($Conf["system"]["session_expire_in_minutes"])*60);
+
+?>
---
+++ GameCardsDev/index/buy.mytcg.net/sms.php
@@ -0,0 +1,20 @@
+<head>
+	<link rel="stylesheet" type="text/css" href="css/stylesheet.css" />
+</head>
+
+<body>
+
+<div id="sms-container">
+	<div id="sms-text-1" class="sms-text">SMS</div>
+	<div id="sms-instructions">mytcg <?php echo $validUserName;?></div>
+	<div id="sms-text-2" class="sms-text">TO</div>
+	<div id="shortcode">36002</div>
+	<div id="sms-text-3" class="sms-text">AND RECEIVE</div>
+	<div class="credit-amount">350 <span>TCG</span><br>credits</div>
+	<div id="sms-text-4" class="sms-text">SMS COST</div>
+	<div id="sms-price">R5.00</div>
+</div>
+<div class="cmdButton" id="closeCheckout">
+	<a href="index.php">Back</a>
+</div>
+</body>
