f4f40785703d878ebaead20f884514e342d1b816
jsinclair <james@mytcg.net>
2013-06-18 14:44:00 +0200

    cycling index
    Signed-off-by: jsinclair <james@mytcg.net>

---
+++ GameCardsDev/index/live/cycling/settings.php
@@ -0,0 +1,14 @@
+<?php
+//kickoff category constant
+include('../functions.php');
+$topcar = "84";
+$root = "../../";
+$invite = 84;
+$usercategories = " AND cx.category_parent_id = 84 ";
+$productcategories = " AND c.category_id = 84 ";
+$url = "www.mytcg.net";
+$appkey = "Cycling";
+$transactionlogtypes = "1,2,3";
+$notificationtypes = "1,2,3";
+$leaderboards = "7";
+?>
